c87aa0bece8c9fe17e421a23707aa01b60baf31e
ben <bwall256@gmail.com>
2016-09-22 03:46:13 +0200

    navigation controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,6 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch',
     'ngMaterial',
   ])
   .config(function ($routeProvider) {
---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendApp.controller:NavCtrl
+ * @description
+ * # NavCtrl
+ * Controller of the frontendApp
+ */
+angular.module('frontendApp')
+  .controller('NavCtrl', function () {
+    var navigationLinks = [];
+
+    function NavigationLinksFact(display, href) {
+      var obj = {};
+      obj.display = display;
+      obj.href = href;
+      navigationLinks.push(obj);
+    }
+
+    // add to scope
+    this.navigationLinks = navigationLinks;
+
+    new NavigationLinksFact('Main', '#/');
+    new NavigationLinksFact('About', '#/about');
+  });
